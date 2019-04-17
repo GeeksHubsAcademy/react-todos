f1f01365e6d816bac8fe2172a7119c3e81a3d8e3
jupegarnica <jupegarnica@gmail.com>
2019-04-17 12:38:11 +0200

    add task logic done

--- src/todos/todos.jsx
+++ src/todos/todos.jsx
@@ -3,17 +3,39 @@ import './todos.scss';
 import Todo from './todo.jsx';
 
 class Todos extends Component {
+  state = {
+    todos: [],
+    newTaskText: '',
+  };
+
+  addTask = text => {
+    if (text) {
+        let newTask = {
+          id: Date.now(),
+          text,
+          completed: false,
+          createdAt: new Date(),
+        };
+
+        this.setState({ todos: [...this.state.todos, newTask] , newTaskText: ''});
+    }
+  };
+  handleNewTask = ev => {
+    if (ev.keyCode === 13) {
+        this.addTask(this.state.newTaskText.trim())
+    }
+  };
+
   render() {
     return (
       <div className='todos'>
         <div className='addTask'>
-          <input type='text' placeholder='add new task' />
+          <input type='text' placeholder='add new task' onKeyUp={this.handleNewTask} value={this.state.newTaskText} onChange={ev => this.setState({ newTaskText: ev.target.value })} />
         </div>
         <div className='taskList'>
-          <Todo text='aprender js' />
-          <Todo text='aprender es6' />
-          <Todo text='aprender react' />
-          <Todo text='aprender redux' />
+          {this.state.todos.map(todo => (
+            <Todo text={todo.text} key={todo.id} />
+          ))}
         </div>
       </div>
     );
